708d45f1dc61dd0f4c0c16ba8c8e4ad1cc6b0303
Ranieri Althoff <ranisalt@gmail.com>
2015-05-22 01:22:12 -0300

    Hardcode some variables (provisionally)

--- fish/config.fish
+++ fish/config.fish
@@ -15,6 +15,9 @@ set fish_plugins theme
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
 
+set -gx CCACHE_SLOPPINESS pch_defines,time_macros
+set -gx CC "ccache gcc"
+set -gx CXX "ccache g++"
 set -gx PATH $HOME/.local/bin $PATH
 set -g TERMINAL "urxvt"
 set -g VIRTUAL_ENV_DISABLE_PROMPT "yes"
