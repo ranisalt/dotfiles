d8dd87ba5ee02238240b377aa04059820486428b
Ranieri Althoff <ranisalt@gmail.com>
2015-07-28 18:45:47 -0300

    More badass fortunes

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,3 +1,6 @@
+# grab quotes with:
+# curl http://api.icndb.com/jokes/?limitTo=\[nerdy\] | jshon -e value -a -e joke -u | sed 's/$/\n%/g' | head -n -1 > chucknorris
+# then move chucknorris to /usr/share/fortune
 function fish_greeting
-	fortune | cowsay -f dragon
+	fortune chucknorris | cowsay -f dragon
 end
