6dae264bea61e78c63d4edc6a8ddae167ffb15a3
Ranieri Althoff <ranisalt@gmail.com>
2015-08-31 03:29:33 -0300

    Add ncmpcpp config

--- install.fish
+++ install.fish
@@ -3,7 +3,7 @@ if [ -z "$XDG_CONFIG_HOME" ]
 	set -x XDG_CONFIG_HOME $HOME/.config
 end
 
-for i in bash conky fish gtk i3 systemd X vim
+for i in bash conky fish gtk i3 ncmpcpp systemd X vim
   pushd $i
   echo "Linking $i files..."
   sh install.sh
