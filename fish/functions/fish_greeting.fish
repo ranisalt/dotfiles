c178b53952d56f9796f0fd9b552ae375beee9597
Ranieri Althoff <ranisalt@gmail.com>
2015-10-11 21:40:54 -0300

    Good bye, my true friend dragon.

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -2,5 +2,5 @@
 # curl http://api.icndb.com/jokes/?limitTo=\[nerdy\] | jshon -e value -a -e joke -u | sed 's/$/\n%/g' | head -n -1 > chucknorris
 # then move chucknorris to /usr/share/fortune
 function fish_greeting
-	fortune chucknorris | cowsay -f dragon
+	# fortune chucknorris | cowsay -f dragon
 end
