238b832190a525d7d9f5c3059a0538ed7ee1a9d6
Ranieri Althoff <ranisalt@gmail.com>
2016-03-11 16:55:00 -0300

    Replace omf with fisherman, remove bash hack

--- fish/config.fish
+++ fish/config.fish
@@ -1,25 +1,14 @@
-# Path to your oh-my-fish.
-set -g OMF_PATH $HOME/.local/share/omf
+set fisher_home ~/.local/share/fisherman
+set fisher_config ~/.config/fisherman
+source $fisher_home/config.fish
 
-# Path to your oh-my-fish configuration.
-set -g OMF_CONFIG $HOME/.config/omf
-
-### Configuration required to load oh-my-fish ###
-# Note: Only add configurations that are required to be set before oh-my-fish is loaded.
-# For common configurations, we advise you to add them to your $OMF_CONFIG/init.fish file or
-# to create a custom plugin instead.
-
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
-
-set -gx CCACHE_SLOPPINESS pch_defines,time_macros
+set -gx CCACHE_SLOPPINESS "pch_defines,time_macros"
 set -gx CC "ccache gcc"
 set -gx CXX "ccache g++"
+set -gx EDITOR "vim"
 set -gx PATH $HOME/.local/bin $PATH
-set -g TERMINAL "urxvt"
+set -gx TERMINAL "urxvt"
 set -g VIRTUAL_ENV_DISABLE_PROMPT "yes"
+set -g theme_date_format "+%R"
 set -g theme_display_hg no
 set -g theme_display_ruby no
-
-test -s /home/ranieri/.nvm-fish/nvm.fish; and source /home/ranieri/.nvm-fish/nvm.fish
-set -gx PATH (echo $HOME/.gem/ruby/(ruby --version | awk '{ print $2 }' | cut -c -4)0/bin) $PATH
