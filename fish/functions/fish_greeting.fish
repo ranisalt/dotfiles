62e134663bd445d0e7d0ad26b04b61e4541ebf94
Ranieri Althoff <ranisalt@gmail.com>
2016-03-21 11:46:14 -0300

    Remove some unused fish functions

--- fish/functions/fish_greeting.fish
+++
@@ -1,6 +0,0 @@
-# grab quotes with:
-# curl http://api.icndb.com/jokes/?limitTo=\[nerdy\] | jshon -e value -a -e joke -u | sed 's/$/\n%/g' | head -n -1 > chucknorris
-# then move chucknorris to /usr/share/fortune
-function fish_greeting
-	# fortune chucknorris | cowsay -f dragon
-end
