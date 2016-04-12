e0feb599aad22bec6bab694f3388ced7955ea2e6
Ranieri Althoff <ranisalt@gmail.com>
2016-04-12 11:51:34 -0300

    Move makepkg config to user dir

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env sh
 [ -z "$XDG_CONFIG_HOME" ] && export XDG_CONFIG_HOME=$HOME/.config
-export RSYNC="rsync -a --info=NAME1 --info=NAME2 --exclude 'install.sh'"
+export RSYNC="rsync -a --info=NAME1 --exclude 'install.sh'"
+
+echo "Installing config files..."
+eval "${RSYNC} ${PWD}/config ${HOME}/.config"
 
 for i in bash fish gtk i3 ncmpcpp systemd X vim; do
   cd $i || exit
-  echo "Installing $i files..."
+  echo "Installing ${i} files..."
   sh install.sh
-  echo
   cd - > /dev/null || exit
 done
 
