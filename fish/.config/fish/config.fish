77bf3470ffbce4a6b8f60dbb1c454df881454b4b
Ranieri Althoff <ranisalt@gmail.com>
2016-08-30 16:02:33 -0300

    Add nodenv to the path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,9 +3,11 @@ set -gx CCACHE_SLOPPINESS "pch_defines,time_macros"
 set -gx CC "ccache gcc"
 set -gx CXX "ccache g++"
 set -gx EDITOR "nvim"
-set -gx PATH $HOME/.local/bin $PATH
+set -gx PATH $HOME/.nodenv/bin $HOME/.local/bin $PATH
 set -gx TERMINAL "urxvt"
 set -g VIRTUAL_ENV_DISABLE_PROMPT "yes"
 set -g theme_date_format "+%R"
 set -g theme_display_hg no
 set -g theme_display_ruby no
+
+status --is-interactive; and . (nodenv init -|psub)
