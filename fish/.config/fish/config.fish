f6f13b653b17e5e00b8283954a81a456473a5be0
Ranieri Althoff <ranisalt@gmail.com>
2016-09-13 16:24:36 -0300

    Add nodenv to the path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,11 @@
 set -gx BROWSER "/usr/bin/firefox-developer"
 set -gx CCACHE_SLOPPINESS "pch_defines,time_macros"
 set -gx EDITOR "nvim"
-set -gx PATH $HOME/.nodenv/bin $HOME/.local/bin $PATH
 set -gx TERMINAL "urxvt"
 set -g VIRTUAL_ENV_DISABLE_PROMPT "yes"
 set -g theme_date_format "+%R"
 set -g theme_display_hg no
 set -g theme_display_ruby no
 
+set -gx PATH $HOME/.nodenv/bin $HOME/.local/bin $PATH
 status --is-interactive; and . (nodenv init -|psub)
