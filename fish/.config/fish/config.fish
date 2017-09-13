dccfd06c40fb4fabc9f8e7373e27c526ea2e5762
Ranieri Althoff <ranisalt@gmail.com>
2017-09-13 18:44:38 -0300

    Update path to add Intel Parallel Studio stuff

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,7 +10,13 @@ set theme_show_exit_status "yes"
 
 # start X at login
 if status --is-login
-    set -x PATH $HOME/.local/bin $HOME/.nodenv/bin $HOME/.pyenv/bin $PATH
+    set -x PATH $HOME/.local/bin $HOME/.nodenv/bin $HOME/.pyenv/bin /usr/local/bin $PATH
+
+    # Intel Parallel Studio XE
+    set -x PATH "$HOME/intel/bin" $PATH
+    set -x PATH "$HOME/intel/compilers_and_libraries_2017.4.196/linux/mpi/intel64/bin" $PATH
+    set -x PATH "$HOME/intel/vtune_amplifier_xe_2017.4.0.517083/bin64" $PATH
+    set -x VTUNE_AMPLIFIER_XE_2017_DIR "$HOME/intel/vtune_amplifier_xe_2017.4.0.517083"
 
     status --is-interactive; and source (nodenv init -|psub)
     status --is-interactive; and source (pyenv init -|psub)
