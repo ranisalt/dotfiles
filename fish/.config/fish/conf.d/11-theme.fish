3ce057e54bbc698fdd69b30384e45af080fd1473
Ranieri Althoff <ranisalt@gmail.com>
2018-04-17 19:47:41 -0300

    Cleanup theme configuration

--- fish/.config/fish/conf.d/11-theme.fish
+++ fish/.config/fish/conf.d/11-theme.fish
@@ -1,5 +1,1 @@
 set fish_prompt_pwd_dir_length 1
-set theme_date_format "+%R"
-set theme_project_dir_length $fish_prompt_pwd_dir_length
-set theme_show_exit_status "yes"
-set VIRTUAL_ENV_DISABLE_PROMPT "yes"
