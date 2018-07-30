a345f952730f7cd87105093ab6c330bcf5fc3b1a
Ranieri Althoff <ranisalt@gmail.com>
2018-07-30 04:25:12 -0300

    Cleanup fish config

--- fish/.config/fish/conf.d/40-pyenv.fish
+++
@@ -1,6 +0,0 @@
-if status --is-login
-  set -x PYENV_ROOT "$HOME/.pyenv"
-  set -x PATH "$PYENV_ROOT/bin" $PATH
-  set -x VIRTUAL_ENV_DISABLE_PROMPT 1
-end
-
--- fish/.config/fish/conf.d/99-startx.fish
+++
@@ -1,4 +0,0 @@
-if status --is-login
-  and test -z "$DISPLAY" -a "$XDG_VTNR" -eq 1
-    exec startx -- -keeptty
-end
