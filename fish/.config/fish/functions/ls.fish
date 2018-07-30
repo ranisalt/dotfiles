725f9a52bf16d630a8d9a443f1241628c9829951
Ranieri Althoff <ranisalt@gmail.com>
2018-07-30 04:25:51 -0300

    Change time format on ls

--- fish/.config/fish/functions/ls.fish
+++ fish/.config/fish/functions/ls.fish
@@ -1,3 +1,4 @@
+# Defined in /tmp/fish.HHueHK/ls.fish @ line 2
 function ls
-	command ls --classify --color=auto --group-directories-first $argv
+	command ls --classify --color=auto --group-directories-first --time-style=+"%d.%m.%Y %H:%M" $argv;
 end
