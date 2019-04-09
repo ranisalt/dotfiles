a3fe80e6a33ec2a63fdef5bbc431be115501f4c7
Ranieri Althoff <ranisalt@gmail.com>
2019-04-09 04:48:55 -0300

    Update wallpaper, move to systemd.target-based i3 setup

--- fish/.config/fish/conf.d/91-wayland.fish
+++ fish/.config/fish/conf.d/91-wayland.fish
@@ -1,3 +1,9 @@
-set -x XKB_DEFAULT_LAYOUT 'br'
-set -x XKB_DEFAULT_MODEL 'abnt2'
-set -x XKB_DEFAULT_OPTIONS 'ctrl:swapcaps'
+if [ -z "$WAYLAND_DISPLAY" ]
+    exit
+end
+# set -x XKB_DEFAULT_LAYOUT 'br'
+# set -x XKB_DEFAULT_MODEL 'abnt2'
+# set -x XKB_DEFAULT_OPTIONS 'ctrl:swapcaps'
+set -x CLUTTER_BACKEND wayland
+set -x QT_QPA_PLATFORM wayland-egl
+set -x SDL_VIDEODRIVER wayland
