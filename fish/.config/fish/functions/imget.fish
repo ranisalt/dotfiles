b165b800d325a46e96cbef367a7e98a506f8f60a
Ranieri Althoff <ranisalt@gmail.com>
2022-09-11 01:30:25 +0200

    Massive cleanup

--- fish/.config/fish/functions/imget.fish
+++
@@ -1,4 +0,0 @@
-# Defined in /tmp/fish.kki9jS/imget.fish @ line 1
-function imget
-	curl -sL -H 'authorization: client-id cc752a847ab1a97' "https://api.imgur.com/3/album/$argv[1]/images" | jq -r '.data[].link' | aria2c --conf-path ~/.config/pacman/aria2.conf -i -;
-end
