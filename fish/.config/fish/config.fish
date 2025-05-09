e02ac67ba7bdfa3dd7d01b759b38ae280a31efa7
Ranieri Althoff <ranisalt@gmail.com>
2025-05-09 18:42:12 +0200

    Update asdf loading

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,18 @@
 set -x fish_greeting
 
 # asdf
-test -f '/opt/asdf-vm/asdf.fish'
-  and source '/opt/asdf-vm/asdf.fish'
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
 
 # vim
 set -x VIMINIT 'if !has("nvim") | let $MYVIMRC="$XDG_CONFIG_HOME/vim/vimrc" | so $MYVIMRC | endif'
